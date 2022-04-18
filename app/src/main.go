1704789a3c8830bb88d8771604be569afd6a7706
ekin.bulut <ekin.bulut@yemeksepeti.com>
2022-04-18 15:01:52 +0300

    main function clear

--- app/src/main.go
+++ app/src/main.go
@@ -5,36 +5,25 @@ import (
 	hservice "github.com/ekinbulut-yemeksepeti/samples/app/hashtable"
 )
 
+type App struct {
+	Name      string
+	Version   string
+	ArrayList *aservice.ArrayList
+	HashTable *hservice.HashTable
+}
+
+func NewApp() *App {
+	return &App{
+		Name:      "App",
+		Version:   "1.0.0",
+		ArrayList: aservice.NewArrayList(),
+		HashTable: hservice.NewHashTable(10),
+	}
+}
+
 func main() {
 
-	// Create a new HashTable.
-	ht := hservice.NewHashTable(10)
-
-	// Add a new key-value pair.
-	ht.Add("key", "value")
-	ht.Add("key2", "value2")
-	ht.Add("key2", "value3")
-
-	ht.Print()
-
-	// create a new arraylist
-	list := aservice.NewArrayList()
-
-	list.Add("value")
-	list.Add("value2")
-	list.Add("value3")
-
-	list.Print()
-
-	s := list.Size()
-
-	println("Size: ", s)
-
-	println("Removing: ")
-	list.Remove(1)
-
-	println("Size: ", s)
-
-	list.Print()
-
+	a := NewApp()
+	a.ArrayList.Add(1)
+	a.ArrayList.Print()
 }
